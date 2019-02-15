a37ffb5413ca68d2486c628dac033ba0a1a1c4d3
vatsal184 <43042897+vatsal184@users.noreply.github.com>
2019-02-15 00:55:53 -0500

    Update env.sv

--- tb/env.sv
+++ tb/env.sv
@@ -1,12 +1,14 @@
 
 `include "agent.sv"
 `include "subscriber.sv"
+`include "scoreboard.sv"
 class riscv_env extends uvm_env;
 
   `uvm_component_utils(riscv_env)
     
   	riscv_agent      agent;
   	riscv_coverage	cov;
+  	riscv_scoreboard sc;
     
     function new(string name, uvm_component parent);
       super.new(name, parent);
@@ -14,11 +16,13 @@ class riscv_env extends uvm_env;
  
     function void build_phase(uvm_phase phase);
       agent = riscv_agent::type_id::create("agent", this);      
-      cov = riscv_coverage::type_id::create("cov", this);
+      cov = riscv_coverage::type_id::create("cov", this);    
+      sc = riscv_scoreboard::type_id::create("sc", this);
     endfunction: build_phase
   	
   function void connect_phase(uvm_phase phase);
     agent.monitor.m_port.connect(cov.sb_port);
+    agent.monitor.m_port.connect(sc.sc_port);
   endfunction: connect_phase
   
 endclass: riscv_env
