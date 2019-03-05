33fc86615b5ef4c1a8690754221eca1cd992b68e
vatsal184 <43042897+vatsal184@users.noreply.github.com>
2019-03-05 16:14:15 -0500

    Update hw_control.sv

--- target/hw_control.sv
+++ target/hw_control.sv
@@ -1,13 +1,13 @@
 module hw_control(
-  	input clk,
   	input reset,
-    output reg [31:0]predecessor,
-    output reg [31:0]successor,
+  	input r_flag,
+    	output reg [31:0]predecessor,
+    	output reg [31:0]successor,
 	input [31:0]pc,
 	input [31:0]Imm_H,
 	input [2:0]funct,
-    input H_sel,
-    input fence,
+    	input H_sel,
+    	input fence,
 	input zero,
 	input less_than,
 	input branch,
@@ -18,8 +18,8 @@ module hw_control(
 	);
   
 
-wire [31:0] pc_in0;
-wire [31:0] pc_in1;
+reg [31:0] pc_in0;
+reg [31:0] pc_in1;
 reg [31:0]prev;
 reg b_type;
 reg pc_sel;
@@ -31,21 +31,21 @@ case ({funct[2],funct[0]})
 	2'b10: b_type = less_than;
   	2'b11: b_type = ~ (less_than | zero);
 endcase
-  pc_sel <= (jump) | (branch & b_type);
+  	pc_sel = (jump) | (branch & b_type);
+    pc_in0 = next_pc;
+    pc_in1 = (jump & ~H_sel) ? alu_out_h : (pc + Imm_H);
+	pc_out = pc_sel ? pc_in1 : pc_in0;
 end  
   
   always @(*) begin
     if(!fence)  prev  = pc;
-  	next_pc = pc + 32'd4 ;
-	pc_out = reset ? (pc_sel ? pc_in1 : pc_in0) : 32'b0;
+  	next_pc = r_flag ? 32'b0 : pc + 32'd4 ;
 end 
   
 always @(fence) begin
-    predecessor <= prev;
-    successor <= next_pc;
+    predecessor = prev;
+    successor = next_pc;
 end
 
-assign pc_in0 = next_pc;
-assign pc_in1 = (jump & ~H_sel) ? alu_out_h : (pc + Imm_H);
   
 endmodule 
