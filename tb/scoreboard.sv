c111413eaecd2f7abd1cc5d6bd159106c1bbc0c6
vatsal184 <43042897+vatsal184@users.noreply.github.com>
2019-03-23 16:23:47 -0400

    Add files via upload

--- tb/scoreboard.sv
+++ tb/scoreboard.sv
@@ -1,3 +1,5 @@
+// Missing - U, ECALL, memdump
+
 
 
 class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
@@ -7,7 +9,7 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
   reg [31:0] stack[31:0];
   reg [31:0] mem[(2**25) - 1:0],	csr[4095:0];
   reg [31:0] dat;
-  reg [31:0] pc, next_pc, reg_rd_dat, m_addr, m_dat, m_addr_obs, m_dat_obs;
+  reg [31:0] pc, next_pc, reg_rd_dat, m_addr, m_dat, m_addr_obs, m_dat_obs, rd_dat, op1, op2, se_b;
   reg bflag = 0, m_flag = 0;
   uvm_analysis_imp #(riscv_seq_item, riscv_scoreboard) sc_port;
 
@@ -26,15 +28,13 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
           
       stack[0] = 32'b0;
    
-        if (bflag) begin		// 	Branch Check
-          if (t.pc === next_pc)	$display("PC STATUS :	 --------- PASS ---------	Expected :	%h		Observed :	%h", next_pc, t.pc); 
-        else	$display("PC STATUS :	 (!) ----------------FAIL 	Expected :	%h		Observed :	%h", next_pc, t.pc); 
+    
+        
+		if (bflag) begin		// 	Branch Check
+            if (t.pc === next_pc)	$display("PC STATUS :	 --------- PASS ---------	Expected :	%h		Observed :	%h", next_pc, t.pc); 
+			else	$display("PC STATUS :	 (!) ----------------FAIL 	Expected :	%h		Observed :	%h", next_pc, t.pc); 
         bflag = 0;
       end
-         
-      
-        
-        
         
         
         
@@ -45,20 +45,19 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
           end
         
        7'b0010111: 	begin//	AUIPC
-          stack[t.instr[11:7]] = {t.instr[31:12], 12'b0} + t.pc;
-        	end
+         stack[t.instr[11:7]] = {t.instr[31:12], 12'b0} + t.pc;
+       	 end
           
           
        	7'b1101111: 	begin//	JAL
           stack[t.instr[11:7]] = 32'd4 + t.pc;
-            pc = t.pc; 
-          next_pc = ({{12{t.instr[31]}},t.instr[19:12], t.instr[20], t.instr[30:21],1'b0} + pc);	// & 32'hfffffffe;
+          next_pc = (t.pc + $signed({{12{t.instr[31]}},t.instr[19:12], t.instr[20], t.instr[30:21],1'b0})) & 32'hfffffffc; 	
+//          $display("%h + %h = %h",{{12{t.instr[31]}},t.instr[19:12], t.instr[20], t.instr[30:21],1'b0} , pc, {{12{t.instr[31]}},t.instr[19:12], t.instr[20], t.instr[30:21],1'b0} + pc);
           	bflag = 1;
         	end
         
         7'b1100111: 	begin//	JALR
-            pc = t.pc;
-          next_pc = ({{21{t.instr[31]}},t.instr[30:20]} + stack[t.instr[19:15]]) & 32'hfffffffe;
+			next_pc = ($signed({{21{t.instr[31]}},t.instr[30:20]}) + $signed(stack[t.instr[19:15]])) & 32'hfffffffc;
           	stack[t.instr[11:7]] = 32'd4 + t.pc;
           	bflag = 1;
 //          $display(" %d + %d",stack[t.instr[19:15]] , {20'b0,t.instr[31:20]} );
@@ -66,84 +65,80 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
         	end 
         
         7'b1100011: 	begin
+         bflag = 1;
+              if (t.instr[13]) begin
+                op1 = stack[t.instr[19:15]];
+                op2 = stack[t.instr[24:20]];
+              end else begin
+                op1 = $signed(stack[t.instr[19:15]]);
+                op2 = $signed(stack[t.instr[24:20]]);
+              end
+			se_b = $signed({{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0});
           
-                bflag = 1;
-          
+//		  $display("op1 = %h, op2 = %h",op1, op2);
+		  
          case(t.instr[14:12])
-           
            3'b000	:	begin	//	BEQ
-             if(stack[t.instr[19:15]]==stack[t.instr[24:20]]) begin
-                pc = t.pc;
-                next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-          	end  
-             else begin
-               pc = t.pc;
-               next_pc = t.pc + 32'd4;
-             end
-         end
+            if(op1 == op2) 	next_pc = se_b + t.pc;
+            else 	next_pc = t.pc + 32'd4;
+			end
            
+           3'b001	:	begin	//	BNE
+            if(op1 == op2) 	next_pc = t.pc + 32'd4;
+            else 	next_pc = se_b + t.pc;
+			end 
            
-           3'b001	:	begin
-             if(stack[t.instr[19:15]]==stack[t.instr[24:20]]) begin	//	BNE
-            	pc = t.pc;
-           		next_pc = t.pc + 32'd4;
-          end   
-             else begin
-               pc = t.pc;
-               next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-             end
-         end 
+           3'b100	:	begin	//	BLT
+            if(op1 < op2)	next_pc = se_b + t.pc;
+            else next_pc = t.pc + 32'd4;
+			end
            
+           3'b101	:	begin	//	BGE
+            if (op1 > op2) next_pc = se_b + t.pc;
+            else next_pc = t.pc + 32'd4;
+			end
            
-           3'b100	:	begin
-             if(stack[t.instr[19:15]] < stack[t.instr[24:20]]) begin	//	BLT
-               pc = t.pc;
-               next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-          end   
-             else begin
-               pc = t.pc;
-               next_pc = t.pc + 32'd4;
-             end
-         end
-           
-           
-           3'b101	:		begin
-             if (stack[t.instr[19:15]] > stack[t.instr[24:20]]) begin	//	BGE
-                 pc = t.pc;
-                 next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-          end 
-             else begin
-               pc = t.pc;
-               next_pc = t.pc + 32'd4;
-             end
-         end
-           
-           3'b110	:	begin
-             if(stack[t.instr[19:15]] < stack[t.instr[24:20]]) begin	//	BLTU        
-                 pc = t.pc;
-                 next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-          		end   
-             else begin
-               pc = t.pc;
-               next_pc = t.pc + 32'd4;
-             end
+           3'b110	:	begin	//	BLTU 
+            if(op1 < op2) next_pc = se_b + t.pc;
+            else next_pc = t.pc + 32'd4;
          	end 
            
-        3'b111	:	begin
-          if(stack[t.instr[19:15]] > stack[t.instr[24:20]]) begin	//	BGEU    
-                 pc = t.pc;
-                 next_pc = {{20{t.instr[31]}},t.instr[7],t.instr[30:25],t.instr[11:8],1'b0} + t.pc;
-          		end  
-             else begin
-               pc = t.pc;
-               next_pc = t.pc + 32'd4;
-             end
-         	end
+           3'b111	:	begin	//	BGEU
+            if(op1 > op2) 	next_pc = se_b + t.pc;
+            else 	next_pc = t.pc + 32'd4;
+			end
          endcase
-                     
+          $display("op1 = %h, op2 = %h",op1, op2);        
+       end
+	   
+       
+       7'b0000011 : begin	//	LOAD
+         m_flag = 0;
+         if (t.instr[14])	m_addr = (stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:20]}) << 2;
+         else 	m_addr = ($signed(stack[t.instr[19:15]]) + $signed({{21{t.instr[31]}},t.instr[30:20]})) << 2;
+         case(t.instr[14:12])
+           3'b000 : 	stack[t.instr[11:7]] = (mem[m_addr]) & 32'h000000ff;
+           3'b001 : 	stack[t.instr[11:7]] = (mem[m_addr]) & 32'h0000ffff;
+           3'b010 : 	stack[t.instr[11:7]] = (mem[m_addr]);
+           3'b100 : 	stack[t.instr[11:7]] = (mem[m_addr]) & 32'h000000ff;
+           3'b101 : 	stack[t.instr[11:7]] = (mem[m_addr]) & 32'h0000ffff;         
+         endcase 
+         if (t.instr[11:7] == 5'b00000)	stack[t.instr[11:7]] = 32'b0;
+         m_dat =  stack[t.instr[11:7]];
        end
        
-        
+	   
+      7'b0100011 : begin	//	STORE
+         m_flag = 1;
+        m_addr = (stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:25],t.instr[11:7]}) << 2;
+         case(t.instr[14:12])
+           3'b000 : 	m_dat = stack[t.instr[24:20]] & 32'h000000ff;
+           3'b001 : 	m_dat = stack[t.instr[24:20]] & 32'h0000ffff;
+           3'b010 : 	m_dat = stack[t.instr[24:20]];
+         endcase
+        mem[m_addr] = m_dat ;
+       end
+                
        
      7'b0010011: begin	// I
          case(t.instr[14:12])
@@ -153,35 +148,36 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
            3'b100	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] ^ {{21{t.instr[31]}},t.instr[30:20]};
            3'b110	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] | {{21{t.instr[31]}},t.instr[30:20]};
            3'b111	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] & {{21{t.instr[31]}},t.instr[30:20]};
-           3'b001	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] << {27'b0,t.instr[24:20]};
+           3'b001	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] << {{21{t.instr[31]}},t.instr[30:20]};
            3'b101	:	begin
-             stack[t.instr[11:7]] = stack[t.instr[19:15]] >> {27'b0,t.instr[24:20]};
-             if(t.instr[30])	stack[t.instr[11:7]][31] = stack[t.instr[19:15]][31];
-           end   
-           
+                         stack[t.instr[11:7]] = stack[t.instr[19:15]] >> {27'b0,t.instr[24:20]};
+                         if(t.instr[30])	stack[t.instr[11:7]][31] = stack[t.instr[19:15]][31];
+                        end
          endcase
-//       if (t.instr[13:12] == 2'b01)         $display("rd1 = %d	Imm = %d	rd = %d", stack[t.instr[19:15]], {27'b0,t.instr[24:20]}, stack[t.instr[11:7]]);
-//     else		$display("rd1 = %d	Imm = %d	rd = %d", stack[t.instr[19:15]], {20'b0,t.instr[31:20]}, stack[t.instr[11:7]]);
-       end
+    if (t.instr[11:7] == 5'b00000)	stack[t.instr[11:7]] = 32'b0;
+       end 
 
+       
+       
      7'b0110011:	begin	//	R
          case(t.instr[14:12])
            3'b000	:	begin
-             if (t.instr[30]) stack[t.instr[11:7]] = stack[t.instr[19:15]] - stack[t.instr[24:20]];
-             else  stack[t.instr[11:7]] = stack[t.instr[19:15]] + stack[t.instr[24:20]];
+             if (t.instr[31:25] == 7'b0100000) stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) - $signed(stack[t.instr[24:20]]);
+             else  stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) + $signed(stack[t.instr[24:20]]);
            end
-           3'b010	:	stack[t.instr[11:7]] = (stack[t.instr[19:15]] < stack[t.instr[24:20]]) ? 32'b1 : 32'b0;
-           3'b011	:	stack[t.instr[11:7]] = (stack[t.instr[19:15]] < stack[t.instr[24:20]]) ? 32'b1 : 32'b0;
-           3'b100	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] ^ stack[t.instr[24:20]];
-           3'b110	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] | stack[t.instr[24:20]];
-           3'b111	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] & stack[t.instr[24:20]];
-           3'b001	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] << stack[t.instr[24:20]];
+           3'b010	:	stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) < $signed(stack[t.instr[24:20]]) ? 32'b1 : 32'b0;
+           3'b011	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] < stack[t.instr[24:20]] ? 32'b1 : 32'b0;
+           3'b100	:	stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) ^ $signed(stack[t.instr[24:20]]);
+           3'b110	:	stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) | $signed(stack[t.instr[24:20]]);
+           3'b111	:	stack[t.instr[11:7]] = $signed(stack[t.instr[19:15]]) & $signed(stack[t.instr[24:20]]);
+           3'b001	:	stack[t.instr[11:7]] = stack[t.instr[19:15]] << stack[t.instr[24:20]][4:0];
            3'b101	:	begin
-             stack[t.instr[11:7]] = stack[t.instr[19:15]] >> stack[t.instr[24:20]];
-             if(t.instr[30])	stack[t.instr[11:7]][31] = stack[t.instr[19:15]][31];
+             stack[t.instr[11:7]] = stack[t.instr[19:15]] >> stack[t.instr[24:20]][4:0];
+             if(t.instr[30])	stack[t.instr[11:7]][31] = stack[t.instr[19:15]][31];  
            	end   
          endcase 
          if (t.instr[11:7] == 5'b00000)	stack[t.instr[11:7]] = 32'b0;
+//       $display("reg[%d] <- %h = %h %h",t.instr[11:7],stack[t.instr[11:7]],  stack[t.instr[19:15]] , stack[t.instr[24:20]]);
        end
                
           
@@ -223,38 +219,8 @@ class riscv_scoreboard extends uvm_subscriber#(riscv_seq_item);
          endcase
     end 
        
-       7'b0000011 : begin	//	LOAD
-         m_flag = 1;
-         m_addr = (stack[t.instr[19:15]] + {20'b0,t.instr[31:20]}) << 2; 
-         case(t.instr[14:12])
-           3'b000 : 	stack[t.instr[11:7]] = (mem[(stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:20]}) << 2]) & 32'h000000ff;
-           3'b001 : 	stack[t.instr[11:7]] = (mem[m_addr]) & 32'h0000ffff;
-           3'b010 : 	stack[t.instr[11:7]] = (mem[(stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:20]}) << 2]);
-           3'b100 : 	stack[t.instr[11:7]] = (mem[(stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:20]}) << 2]) & 32'h000000ff;
-           3'b101 : 	stack[t.instr[11:7]] = (mem[(stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:20]}) << 2]) & 32'h0000ffff;         
-         endcase 
-         if (t.instr[11:7] == 5'b00000)	stack[t.instr[11:7]] = 32'b0;
-         m_dat =  stack[t.instr[11:7]];
-       end
-       
-      7'b0100011 : begin	//	STORE
-         m_flag = 1;
-        m_addr = (stack[t.instr[19:15]] + {{21{t.instr[31]}},t.instr[30:25],t.instr[11:7]}) << 2;
-         case(t.instr[14:12])
-           3'b000 : 	begin
-             m_dat = stack[t.instr[24:20]] & 32'h000000ff;
-           end
-           3'b001 : 	begin
-             m_dat = stack[t.instr[24:20]] & 32'h0000ffff;
-           end
-           3'b010 : 	begin
-             m_dat = stack[t.instr[24:20]];
-           end
-         endcase
-        mem[m_addr] = m_dat ;
-       end
-        
     endcase  
+       
        
         
         
